786102ca33a8d47c6a9856f2f645eaecf2d8c95b
Klinnex <yann.deboeuf@gmail.com>
2026-05-11 21:15:47 +0200

    feat: add waitingOvertime field to ArenaUpdate

--- src/shared/types/remote.ts
+++ src/shared/types/remote.ts
@@ -152,6 +152,7 @@ export interface ArenaUpdate {
   cardsB?: string[];
   suddenDeath?: boolean;
   overtimeType?: string | null;
+  waitingOvertime?: boolean;
   showPhotos?: boolean; // afficher les photos avant le combat
   cardAnnounce?: boolean; // annoncer les cartons avec raison sur les affichages
   theme?: DisplayTheme; // thème visuel de l'affichage distant
